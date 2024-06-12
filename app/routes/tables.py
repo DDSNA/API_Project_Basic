678dcbb97d7c9026dfb4c7928408daa4efee0a89
DDSNA <79444147+DDSNA@users.noreply.github.com>
2024-06-13 00:34:51 +0300

    add route /tables/list for getting all tables in the schema of interest for PrUn

--- app/routes/tables.py
+++ app/routes/tables.py
@@ -19,12 +19,14 @@ if mode == "dev":
 else:
     pass
 
-@router.get("/tables/{table_name}")
+@router.get("/tables/{table_name}", tags=['functional', 'prun'])
 async def get_table(table_name: str):
     """
     Function in charge of getting tables. Tables with spaces need to have quotes inserted.
 
-    Example: "Average Prices (All)" or _
+    ==Example==: "Average Prices (All)" or _fact_companies_summary_dated
+
+
     :param table_name:
     :return:
     """
@@ -45,5 +47,31 @@ async def get_table(table_name: str):
             return StreamingResponse(table, media_type="text/csv")
     except FileNotFoundError:
         raise HTTPException(status_code=404, detail="Table not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.get("/tables/list")
+async def get_list_tables():
+    try:
+        logging.info(f"{sql_alchemy_postgres_db}, "
+                     f"{sql_alchemy_postgres_host}, "
+                     f"{sql_alchemy_postgres_port}, "
+                     f"{sql_alchemy_postgres_schema},"
+                     f"{sql_alchemy_postgres_db},"
+                     f"{sql_alchemy_postgres_user}")
+        engine = create_engine(f"postgresql://{sql_alchemy_postgres_user}:{sql_alchemy_postgres_password}@{sql_alchemy_postgres_host}:{sql_alchemy_postgres_port}/{sql_alchemy_postgres_db}")
+        with engine.connect() as connection:
+            table = connection.execute(text(
+                                            f"SELECT TABLE_NAME"
+                                            f"FROM INFORMATION_SCHEMA.TABLES"
+                                            f"WHERE TABLE_SCHEMA='prun_data'"
+                                            ))
+            with open (f"table_list.csv", "w") as file:
+                for row in table:
+                    file.write(",".join([str(cell) for cell in row]) + "\n")
+            table = open(f"table_list.csv", "r")
+            return StreamingResponse(table, media_type="text/csv")
+    except FileNotFoundError:
+        raise HTTPException(status_code=404, detail="Table list not found")
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
