a8963f00936f18b49fc63e92e8329ec0423b57f4
DDSNA <79444147+DDSNA@users.noreply.github.com>
2024-06-13 09:24:58 +0300

    add logger to /routes/tables

--- app/routes/tables.py
+++ app/routes/tables.py
@@ -6,7 +6,18 @@ from sqlalchemy import text
 from sqlalchemy.exc import IntegrityError, NoSuchTableError
 import os
 
-from ..app.main import logger
+import logging
+import sys
+
+#logging
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
+
+
 
 router = APIRouter()
 mode = os.environ.get("MODE")
