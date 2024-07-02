ce598e9d218976071c8a0db751670f215e88702a
DDSNA <79444147+DDSNA@users.noreply.github.com>
2024-07-03 00:14:54 +0300

    corrected faulty import

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,14 @@ FROM python:3.10
 
 EXPOSE 80
 
-WORKDIR /code
+WORKDIR /app
 
-COPY ./requirements.txt /code/requirements.txt
+COPY ./requirements.txt /app/requirements.txt
 
-RUN pip install --no-cache-dir --upgrade -r /code/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
 
-COPY ./app ./code/app
+COPY ./app /app
 
-CMD ["uvicorn", "code.app.main:app", "--host", "0.0.0.0", "--port", "80"]
+CMD ["uvicorn", "main:app", "--host", "0.0.0.0", "--port", "80"]
 
 LABEL authors="dan.serban"
--- app/routes/users.py
+++ app/routes/users.py
@@ -1,26 +1,34 @@
+import logging
+import sys
+
 import jwt
 from fastapi import APIRouter, Depends, HTTPException
 import os
 
-from ..main import oauth2_scheme, logger
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
 
 router = APIRouter()
 mode = os.environ.get("MODE")
 
-def verify_token(token: str = Depends(oauth2_scheme)):
-    try:
-        secret_key = os.getenv("SECRET_AUTK_KEY")
-    except Exception as e:
-        print("Error retrieving auth secret key from os")
-        logger.error("Invalid auth key from OS env")
-        raise HTTPException(500, "Improper env setup - contact administrator")
-    try:
-        jwt.decode(token, secret_key, algorithms=['HS256'])
-        return True
-    except jwt.PyJWTError as e:
-        logger.error(f"PyJWT error: {e}")
-        raise HTTPException(status_code=401, detail="Invalid token")
+# oauth2_scheme = "Bearer"
+#
+#
+# def verify_token(token: str = Depends(oauth2_scheme)):
+#     try:
+#         secret_key = os.getenv("SECRET_AUTK_KEY")
+#     except Exception as e:
+#         print("Error retrieving auth secret key from os")
+#         logger.error("Invalid auth key from OS env")
+#         raise HTTPException(500, "Improper env setup - contact administrator")
+#     try:
+#         jwt.decode(token, secret_key, algorithms=['HS256'])
+#         return True
+#     except jwt.PyJWTError as e:
+#         logger.error(f"PyJWT error: {e}")
+#         raise HTTPException(status_code=401, detail="Invalid token")
 
-@router.get("/secure_data")
-async def get_secured_data(valid_token: bool = Depends(verify_token)):
-    return {"message":"Authorized access to secure data"}
+#
+# @router.get("/secure_data")
+# async def get_secured_data(valid_token: bool = Depends(verify_token)):
+#     return {"message": "Authorized access to secure data"}
